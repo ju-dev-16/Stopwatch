81908772299b854a20380a55ead4e91bdf11817d
JuDev <jahidudd65@gmail.com>
2021-10-08 17:34:53 +0200

    Update

--- application/main.js
+++ application/main.js
@@ -1,25 +1,49 @@
-var numerator = document.getElementById("numerator");
-var i = 0;
+var timer;
+var action = false;
+var action_2 = false;
+var i;
 
-function interval() {
-    numerator.innerHTML = i;
-    i++;
+window.onload = function() {
+    numerator = document.getElementById("numerator");
 }
 
-function startInterval() {
-    const time = setInterval(interval, 1000);
-    return time
+function start() {
+    i = 0;
+
+    setInterval(loop, 1000);
 }
 
-function start(action = true) {
-    if (action == true) {
-        startInterval();
-    } else {
-        alert("Error...");
-    }
+function stop() {
+    action_2 = true;
+    setTimeout(check, 1000);
 }
 
 function reset() {
-    clearInterval(startInterval);
-    numerator.innerHTML = 0;
+    action = true;
+}
+
+function loop() {
+    if (action === false) {
+        numerator.innerHTML = i;
+        i++
+    } else if (action === true) {
+        numerator.innerHTML = 0;
+        i = 0;
+        clearInterval(loop);
+    } else {
+        console.log("Error")
+    }
+}
+
+function check() {
+    if (action_2 === false) {
+        numerator.innerHTML = i;
+        i++
+    } else if (action_2 === true) {
+        clearInterval(loop);
+        i = i;
+        numerator.innerHTML = i;
+    } else {
+        console.log("Error")
+    }
 }
